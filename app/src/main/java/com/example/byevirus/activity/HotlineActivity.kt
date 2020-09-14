46958b87ccf6bcefaab5590bcac3c92ea9f5aae2
William Chang <william_chang80@rocketmail.com>
2020-09-15 01:39:52 +0700

    Add lookup skeleton loading

--- app/src/main/java/com/example/byevirus/activity/HotlineActivity.kt
+++ app/src/main/java/com/example/byevirus/activity/HotlineActivity.kt
@@ -4,19 +4,22 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.byevirus.model.Hotline
+import androidx.recyclerview.widget.RecyclerView
+import com.ethanhua.skeleton.Skeleton
+import com.ethanhua.skeleton.SkeletonScreen
 import com.example.byevirus.R
-import kotlinx.android.synthetic.main.activity_hotline_bottom_sheet.*
 import com.example.byevirus.adapter.HotlineAdapter
 import com.example.byevirus.constants.ApiUrl.Companion.HOTLINES_API_URL
+import com.example.byevirus.model.Hotline
+import kotlinx.android.synthetic.main.activity_hotline_bottom_sheet.*
 import okhttp3.*
 import org.json.JSONArray
 import java.io.IOException
 
-class HotlineActivity: AppCompatActivity() {
 
+class HotlineActivity : AppCompatActivity() {
 
-
+    private var skeletonScreen: SkeletonScreen? = null
     private val mockHotlineList = mutableListOf(
         Hotline(
             name = "Loading...",
@@ -35,9 +38,8 @@ class HotlineActivity: AppCompatActivity() {
         rvhotline.adapter = hotlineAdapter
 
 
-
         val request: Request = Request.Builder()
-            .url(HOTLINES_API_URL )
+            .url(HOTLINES_API_URL)
             .build()
         //new call buat request yang di prepare sama okhttp dan enqueue buat jalanin
         okHttpClient.newCall(request).enqueue(getCallback(hotlineAdapter))
@@ -85,4 +87,4 @@ class HotlineActivity: AppCompatActivity() {
         }
     }
 
-    }
+}
--- app/src/main/java/com/example/byevirus/activity/LookUpActivity.kt
+++ app/src/main/java/com/example/byevirus/activity/LookUpActivity.kt
@@ -6,6 +6,9 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ethanhua.skeleton.Skeleton
+import com.ethanhua.skeleton.SkeletonScreen
 import com.example.byevirus.R
 import com.example.byevirus.model.LookUp
 import kotlinx.android.synthetic.main.activity_look_up.*
@@ -20,9 +23,9 @@ import java.lang.Exception
 class LookUpActivity : AppCompatActivity() {
     companion object {
         const val Lookup = "LookUp"
-
     }
 
+    private var skeletonScreen: SkeletonScreen? = null
 
     private val mockLookUpList = mutableListOf(
         LookUp(
@@ -31,7 +34,6 @@ class LookUpActivity : AppCompatActivity() {
             numberOfRecoveredCases = " ",
             numberOfDeathCases = " "
         ),
-
     )
 
     private val okHttpClient = OkHttpClient()
@@ -45,60 +47,67 @@ class LookUpActivity : AppCompatActivity() {
         val lookUpAdapter = LookUpAdapter(mockLookUpList)
         rvlookup.layoutManager = LinearLayoutManager(this)
         rvlookup.adapter = lookUpAdapter
+        val recyclerView = findViewById<RecyclerView>(R.id.rvlookup)
+        skeletonScreen = Skeleton.bind(recyclerView)
+            .adapter(lookUpAdapter)
+            .load(R.layout.lookup_view_skeleton)
+            .count(7)
+            .show()
 
         arrow_click_back.setOnClickListener {
             backToMainPage()
         }
         val request: Request = Request.Builder()
-            .url(LOOKUP_API_URL )
+            .url(LOOKUP_API_URL)
             .build()
         okHttpClient.newCall(request).enqueue(getCallback(lookUpAdapter))
 
     }
+
     private fun backToMainPage() {
         val intent = Intent(this, MainActivity::class.java).apply {
             putExtra(Lookup, "this is from look up activity")
         }
         startActivity(intent)
     }
-        private fun getCallback(lookUpAdapter: LookUpAdapter): Callback{
-            return object: Callback{
-                override fun onFailure(call: Call, e: IOException) {
-                    this@LookUpActivity.runOnUiThread{
-                        Toast.makeText(this@LookUpActivity, e.message,Toast.LENGTH_SHORT).show()
-                    }
+
+    private fun getCallback(lookUpAdapter: LookUpAdapter): Callback {
+        return object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                this@LookUpActivity.runOnUiThread {
+                    Toast.makeText(this@LookUpActivity, e.message, Toast.LENGTH_SHORT).show()
                 }
+            }
 
-                override fun onResponse(call: Call, response: Response) {
-                    try {
-                        val jsonString: String? = response.body?.string()
-                        val jsonArray = JSONArray(jsonString)
-                        val lookUpListFromNetwork: MutableList<LookUp> = mutableListOf<LookUp>()
+            override fun onResponse(call: Call, response: Response) {
+                try {
+                    val jsonString: String? = response.body?.string()
+                    val jsonArray = JSONArray(jsonString)
+                    val lookUpListFromNetwork: MutableList<LookUp> = mutableListOf<LookUp>()
 
-                        for (i in 0 until jsonArray.length()){
-                            //krn di json ada object attributes jadi harus di passing
-                            val attribute = jsonArray.getJSONObject(i).getJSONObject("attributes")
-                            lookUpListFromNetwork.add(
-                                LookUp(
-                                    provinceName = attribute.getString("Provinsi"),
-                                    numberOfPositiveCases = attribute.getString("Kasus_Posi"),
-                                    numberOfRecoveredCases = attribute.getString("Kasus_Semb"),
-                                    numberOfDeathCases = attribute.getString("Kasus_Meni")
-                                )
+                    for (i in 0 until jsonArray.length()) {
+                        val attribute = jsonArray.getJSONObject(i).getJSONObject("attributes")
+                        lookUpListFromNetwork.add(
+                            LookUp(
+                                provinceName = attribute.getString("Provinsi"),
+                                numberOfPositiveCases = attribute.getString("Kasus_Posi"),
+                                numberOfRecoveredCases = attribute.getString("Kasus_Semb"),
+                                numberOfDeathCases = attribute.getString("Kasus_Meni")
                             )
-                        }
-                        this@LookUpActivity.runOnUiThread{
-                            lookUpAdapter.updateData(lookUpListFromNetwork)
-                        }
+                        )
                     }
-                    catch (e: Exception){
-                        this@LookUpActivity.runOnUiThread{
-                            Toast.makeText(this@LookUpActivity, e.message, Toast.LENGTH_SHORT).show()
-                        }
+                    this@LookUpActivity.runOnUiThread {
+                        skeletonScreen?.hide()
+                        lookUpAdapter.updateData(lookUpListFromNetwork)
+                    }
+                } catch (e: Exception) {
+                    this@LookUpActivity.runOnUiThread {
+                        Toast.makeText(this@LookUpActivity, e.message, Toast.LENGTH_SHORT).show()
                     }
                 }
             }
         }
+    }
 
 
 }
